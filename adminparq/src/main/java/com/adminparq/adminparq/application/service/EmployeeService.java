0334ee882172f55676da0f701fa1ae20fbdeb5ee
Valentina Velasquez Botero <valentinavelazquez172@gmail.com>
2023-03-13 12:45:17 -0500

    comentarios

--- adminparq/src/main/java/com/adminparq/adminparq/application/service/EmployeeService.java
+++ adminparq/src/main/java/com/adminparq/adminparq/application/service/EmployeeService.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
-@Slf4j
-@Service
+@RequiredArgsConstructor /*Genera un constructor con un parametro para cada campo que requiere
+un manejo especial */
+@Slf4j /*simple logging facade*/
+@Service /*construir una clase de servicio que habitualmente se conecta a varios repositorios
+y agrupa su funcionalidad */
 public class EmployeeService {
 
   private final EmployeeRepository EmployeeRepository;
--- adminparq/src/main/java/com/adminparq/adminparq/domain/Employee.java
+++ adminparq/src/main/java/com/adminparq/adminparq/domain/Employee.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter /*obtener el valor de una propiedad de la clase y asi poder utilizar
+dicho valor en diferentes metodos */
+@Setter /*su funcion permite brindar acceso a propiedades especificas para poder asignar
+un valor fuera de la clase */
+@AllArgsConstructor /*genera un constructor para todos los atributos de la clase.*/
+@NoArgsConstructor /*genera un constructor sin argumentos, uno vacío */
 public class Employee {
 
   private Long id;
